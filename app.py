8e4c755c1cf5615c6f5f35b29d667f916da66498
harsh <chaturvediharsh123@gmail.com>
2026-06-04 22:19:36 +0530

    add langchain

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-BASE_URL = "https://ai-powered-hr-assistant.onrender.com"
+BASE_URL = "http://127.0.0.1:8000"
 
 st.set_page_config(
     page_title="AI Employee Management System",
